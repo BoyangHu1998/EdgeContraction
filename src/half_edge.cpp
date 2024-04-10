58c8117c62396e3d6465efab30f3afcd559f9095
Boyang Hu <u7565346@anu.edu.au>
2024-04-11 08:46:34 +1000

    Initial commit

---
+++ src/half_edge.cpp
@@ -0,0 +1,101 @@
+#include <include/half_edge.hpp>
+#include <Eigen/Dense>
+
+/*
+#################################################################################
+#                       Vertex-related Helper Functions                         #
+#################################################################################
+*/
+
+// Optinal TODO: Iterate through all neighbour vertices around the vertex
+// Helpful when you implement the average degree computation of the mesh
+std::vector<std::shared_ptr<Vertex>> Vertex::neighbor_vertices() {
+    std::vector<std::shared_ptr<Vertex>> neighborhood;
+
+    return neighborhood; 
+}
+
+
+// TODO: Iterate through all half edges pointing away from the vertex
+std::vector<std::shared_ptr<HalfEdge>> Vertex::neighbor_half_edges() {
+    std::vector<std::shared_ptr<HalfEdge>> neighborhood;
+
+    return neighborhood;
+}
+
+
+// TODO: Computate quadratic error metrics coefficient, which is a 5-d vector associated with each vertex
+/*
+    HINT:
+        Please refer to homework description about how to calculate each element in the vector.
+        The final results is stored in class variable "this->qem_coff"
+*/
+void Vertex::compute_qem_coeff() {
+    this->qem_coff = Eigen::VectorXf(5);
+}
+
+
+/*
+#################################################################################
+#                         Face-related Helper Functions                         #
+#################################################################################
+*/
+
+// TODO: Iterate through all member vertices of the face
+std::vector<std::shared_ptr<Vertex>> Face::vertices() {
+    std::vector<std::shared_ptr<Vertex>> member_vertices;
+
+    return member_vertices;
+}
+
+
+// TODO: implement this function to compute the area of the triangular face
+float Face::get_area(){
+    float area;
+
+    return area;
+}
+
+// TODO: implement this function to compute the signed volume of the triangular face
+// reference: http://chenlab.ece.cornell.edu/Publication/Cha/icip01_Cha.pdf eq.(5)
+float Face::get_signed_volume(){
+    float volume;
+
+    return volume;
+}
+
+
+/*
+#################################################################################
+#                         Edge-related Helper Functions                         #
+#################################################################################
+*/
+
+/*
+    TODO: 
+        Compute the contraction information for the edge (v1, v2), which will be used later to perform edge collapse
+            (i) The optimal contraction target v*
+            (ii) The quadratic error metrics QEM, which will become the cost of contracting this edge
+        The final results is stored in class variable "this->verts_contract_pos" and "this->qem"
+    Please refer to homework description for more details
+*/
+void Edge::compute_contraction() {
+    this->verts_contract_pos = Eigen::Vector3f(0, 0, 0);
+    this->qem = 0;
+}
+
+
+/*
+    TODO: 
+        Perform edge contraction functionality, which we write as (v1 ,v2) → v*, 
+            (i) Moves the vertex v1 to the new position v*, remember to update all corresponding attributes,
+            (ii) Connects all incident edges of v1 and v2 to v*, and remove the vertex v2,
+            (iii) All faces, half edges, and edges associated with this collapse edge will be removed.
+    HINT: 
+        (i) Pointer reassignments
+        (ii) When you want to remove mesh components, simply set their "exists" attribute to False
+    Please refer to homework description for more details
+*/
+void Edge::edge_contraction() {
+    
+}
